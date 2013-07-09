87ce50245f8fdccfec48ba2b7f285a7070402827
reverse <leqiaop1@sina.com>
2013-07-09 14:38:20 +0200

    Initialize the assignment with makefile and passed with make all

---
+++ Assign3/test_bst.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <stdio.h>
+#include "bst.h"
+
+int main() {
+ 
+  bst_t* cur = make_tree(32);
+  init_tree(cur);
+
+ /* Insert 1. */
+  insert(1, cur);
+  assert(find_val(1, cur) == TRUE);
+  print_bst(cur); printf("\n");
+
+  /* Insert 0. */
+  insert(0, cur);
+  assert(find_val(0, cur) == TRUE);
+  print_bst(cur); printf("\n");
+
+  /* Insert 2. */
+  insert(2, cur);
+  assert(find_val(2, cur) == TRUE);
+  print_bst(cur); 
+  printf("\n ----- \n");
+
+  /* Insert 4. */
+  insert(4, cur);
+  assert(find_val(4, cur) == TRUE);
+  print_bst(cur); printf("\n");
+
+  /* Insert 3 */
+  insert(3, cur);
+  assert(find_val(3, cur) == TRUE);
+  print_bst(cur);
+  printf("\n");
+
+  /* ADD YOUR TESTS HERE. */
+  
+  /* Delete the list. */
+  delete_bst(cur);
+
+  return 0;
+}
